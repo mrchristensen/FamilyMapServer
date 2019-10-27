57ff46f23f754ebf4d03871cfd2d7a1b3f3663f0
Matt <mr.christensen@gmail.com>
2019-10-26 22:05:22 -0600

    Json Deserialization (using Gson)

---
+++ src/main/java/JsonDeserialization.java
@@ -0,0 +1,7 @@
+import com.google.gson.Gson;
+
+public class JsonDeserialization {
+    public static <T> T deserialize(String value, Class<T> returnType) {
+        return (new Gson()).fromJson(value, returnType);
+    }
+}
